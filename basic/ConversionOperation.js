d0529d4da0b9b53d284fa97f8b4ed8de050429a9
brajeshsin <119887170+brajeshsin@users.noreply.github.com>
2024-06-07 13:28:29 +0000

    Conversion of data types

---
+++ basic/ConversionOperation.js
@@ -0,0 +1,30 @@
+//let score = 33
+//let age = "33def"
+
+
+// console.log(typeof score);
+// console.log(typeof age);
+
+/*let valueInNumber = Number(age)
+console.log(typeof age);
+console.log(valueInNumber);// output- NaN not a number.
+//"33"=> 33
+//"33abc"=> NaN
+//true=> 1; false=>0
+
+
+let isloggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+*/
+
+//1=> true, 0=> false
+// "" => false
+//  "Brajesh"=> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
