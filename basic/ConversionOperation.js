cf8cc19ab8d4112cae3b75208a1bedfbc1792380
brajeshsin <119887170+brajeshsin@users.noreply.github.com>
2024-06-07 14:55:16 +0000

    Conversion

--- basic/ConversionOperation.js
+++ basic/ConversionOperation.js
@@ -23,8 +23,37 @@ console.log(booleanIsLoggedIn);
 // "" => false
 //  "Brajesh"=> true
 
-let someNumber = 33
+/*let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+
+// **********************************************--OPERATIONS--*********************************************************************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(18%5);
+
+// let str1="hello"
+// let str2="brajesh"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);// if sting first then conversion will be according to string
+// console.log(1 + "2");
+// console.log("1" + 2+2);
+// console.log(1 + 2+"3");//If number first then coversion will be according to number
+
+console.log(+true);// output= 1
+
+
+
+
