0196d6227f78b77d23318a75573d91f173f021df
brajeshsin <119887170+brajeshsin@users.noreply.github.com>
2024-06-11 15:12:13 +0000

    function

---
+++ 2_basic/function.js
@@ -0,0 +1,14 @@
+
+
+function sayMyName(){
+    console.log("h");
+console.log("b");
+}
+
+sayMyName() // Function execution
+
+function addTwoNumbers(number1,number2){
+           let result = number1 + number2
+           return result
+}
+ const result = addTwoNumbers(3,4)
